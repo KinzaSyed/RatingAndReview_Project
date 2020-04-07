b5c554509eb13fbb9e84809a328d446fa90c684c
KinzaSyed <kinzasyed247@gmail.com$   git config --global user.email kinzasyed247@gmail.com>
2020-04-07 14:51:52 +0500

    Add project files.

---
+++ RatingAndReview_Project/Controllers/AccountsController.cs
@@ -0,0 +1,56 @@
+﻿using RatingAndReview_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RatingAndReview_Project.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private RatingAndReviewEntities db = new RatingAndReviewEntities();
+
+        // GET: Accounts
+        
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var count = db.Accounts.Count(a => a.Username.Equals(username) && a.APassword.Equals(password));
+            if (count > 0)
+            {
+                Session["username"] = username;
+                return RedirectToAction("Index", "Product");
+            }
+
+            else
+            {
+                ViewBag.error = "Invalid Account";
+                return View("Login");
+            }
+            
+        }
+
+        
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View("SignUp");
+        }
+        [HttpPost]
+        public ActionResult SignUp(Account account)
+        {
+            db.Accounts.Add(account);
+            db.SaveChanges();
+
+            return RedirectToAction("Login","Accounts");
+        }
+
+
+
+    }
+}
---
+++ RatingAndReview_Project/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using RatingAndReview_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RatingAndReview_Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        private RatingAndReviewEntities db = new RatingAndReviewEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            ViewBag.products = db.Products.ToList();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            ViewBag.product = db.Products.Find(id);
+            return View();
+        }
+    }
+}
